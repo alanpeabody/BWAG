c7edc89da1f86a87007ee140dc4a40bf6b9f5e9e
Alan Peabody <alan@alan-laptop.(none)>
2010-05-22 12:46:55 -0400

    <strike>Burlington Beer Drinkers Alliance</strike> Burlington Web Application Group web application initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'cffc5fae793c13a041467a5bc4ec73c6ddc40aa55f8b4b405bad602b29011c9220fe49abe68099796e64a5364b1827df9b3f3c4228b16ac0e86f62e10e2e9efb'
