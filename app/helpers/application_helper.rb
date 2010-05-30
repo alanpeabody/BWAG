06305a758fbf56fd49fe6decc801e41fdb71c732
Alan Peabody <gapeabody@gmail.com>
2010-05-30 00:06:28 -0400

    Added flash helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  # Renders Flash Notices
+  def display_flash
+    [:alert,:notice].collect do |type|
+      content_tag(:div, flash[type], :class => [type.to_s,"flash"]) if flash[type]
+    end.compact.join("\n").html_safe
+  end
+  
 end
